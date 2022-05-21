c234ae7a021a79a3298c10cc6eeb7623ba08ae03
joyjsmun <joy.js.mun@gmail.com>
2022-05-21 10:54:18 +0900

    ADD: first commit

---
+++ src/theme.ts
@@ -0,0 +1,16 @@
+import {DefaultTheme} from "styled-components"
+
+
+export const darkTheme:DefaultTheme = {
+    bgColor:"#f53b57",
+    fontColor:"#f65555",
+    blockColor:"#ffffcd",
+    accentColor:"#feca57"
+}
+
+export const lightTheme:DefaultTheme = {
+    bgColor:"white",
+    fontColor:"white",
+    blockColor:"#f53b57",
+    accentColor:"#feca57"
+}
