0dc1c67055fbf1b5c30768c1eab6b2bdee3b386e
joyjsmun <joy.js.mun@gmail.com>
2022-05-21 10:58:56 +0900

    ADD: theme setting

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RecoilRoot } from 'recoil';
 import App from './App';
+
 
 
 const root = ReactDOM.createRoot(
@@ -8,6 +10,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+    <RecoilRoot>
     <App />
+    </RecoilRoot>
   </React.StrictMode>
 );
